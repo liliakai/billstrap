5f1105e3a99be57430dc5e8b6adaa140ac5b3401
Lilia Markham <lilia.markham@causes.com>
2013-06-14 23:34:06 -0700

    rails new cohorts -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cohorts::Application.config.secret_token = 'a05809226c280d46f073c5ee2589afc6cdf13d9925b9ac912405af3f7f98d90d6a0d60cc63d3743eda121d92467a3de31fb6de553fffd9ce44eb60b1b0881117'
